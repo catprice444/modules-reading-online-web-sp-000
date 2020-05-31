1c67564f73ea3d039e5703dfe6dbdf76af18a00b
Catherine Price <catprice444@gmail.com>
2020-05-31 15:30:57 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,13 +1,7 @@
-module ClassMethods
+module MetaDancing
 
   def metadata
     "This class produces objects that love to dance."
   end
 
-  module MetaDancing
-
-    def metadata
-      "This class produces objects that love to dance."
-    end
-  end
 end
