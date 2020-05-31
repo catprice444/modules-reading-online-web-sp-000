a3658d8550e342d3dd16e02c635ed790bfe7b51d
Catherine Price <catprice444@gmail.com>
2020-05-31 15:32:17 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,6 +5,12 @@ require_relative './fancy_dance.rb'
 class Kid
   include Dance
   extend MetaDancing
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
